cdc04f6cc1e98aaf38a4db3c5db03a4c21ac26f4
FirstFox2 <127059545+FirstFox2@users.noreply.github.com>
2023-08-25 15:07:08 -0400

    Update websiteIPFinder_V3.py
    Allow for directories and https or http

--- websiteIPFinder_V3.py
+++ websiteIPFinder_V3.py
@@ -2,6 +2,9 @@ import socket
 
 while 1:
     website = input("Please enter the URL:\n")
+    if website.__contains__("https://"): website = website[8:]
+    elif website.__contains__("http://"): website = website[7:]
+    if website.__contains__("/"): website = website[:website.index("/")]
     if not website.__contains__(".") or website[len(website) - 1] == ".": continue
     try:
         print(socket.getaddrinfo(website, 0)[0][4][0])
